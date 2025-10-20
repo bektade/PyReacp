98ad584098c2704585e32050aad9fc39d2a242bd
Bekalu Tadesse <bekpostgrad@gmail.com>
2025-10-19 22:54:05 -0500

    list indx & slicing

---
+++ PurePython/Iterables/Lists/indexingLists.py
@@ -0,0 +1,21 @@
+"""
+
+# negative indexes [-3, -2, -1]
+# the right most is -1 for neg index
+# when slicing the last element gets ignored
+# slicing returns a list 
+# indexing returns a number
+
+"""
+
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[0:2])
+
+
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-3:-1])
