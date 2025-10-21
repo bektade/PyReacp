9adc6b3ba08be00e7b5affab4cf05cdecfcfb3ed
Bekalu Tadesse <bekpostgrad@gmail.com>
2025-10-20 21:45:29 -0500

    extending lists - adding lists

---
+++ PurePython/Iterables/Lists/extendList.py
@@ -0,0 +1,30 @@
+"""
+
+
+Extend () : 
+
+To append elements from another list to the current list
+
+
+We could also add them!
+
+"""
+
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+
+
+
+
+
+ls1 = ["A", "B"]
+
+ls2 = [3, "D"]
+
+ls3 = ls1 + ls2
+
+print(ls3)
