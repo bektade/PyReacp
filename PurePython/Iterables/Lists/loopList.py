d840a8de34d9731ab467b2f96629dd7fbe333b88
Bekalu Tadesse <bekpostgrad@gmail.com>
2025-10-22 00:00:20 -0500

    loopList

---
+++ PurePython/Iterables/Lists/loopList.py
@@ -0,0 +1,20 @@
+thislist = ["apple", "banana", "cherry"]
+
+
+
+for x in thislist:
+	print(x)
+
+
+for i in range(len(thislist)):
+  print(thislist[i], i)
+
+
+
+i = 1
+while i < len(thislist):
+  print(thislist[i], i)
+  i = i + 1
+
+
+[print(x) for x in thislist]
